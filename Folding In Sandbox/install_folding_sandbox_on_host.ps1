d3b8b88bbdc6c734df9a2d77b8527cef30451acc
Cole Goeppinger <cole@ronzer.org>
2020-04-09 10:43:05 -0700

    Fix - BIOS check is actually checking for Hyper-V
    Make separate CIM checks for VirtualizationFirmwareEnabled and HypervisorPresent

--- Folding In Sandbox/install_folding_sandbox_on_host.ps1
+++ Folding In Sandbox/install_folding_sandbox_on_host.ps1
@@ -9,8 +9,15 @@ $ProgressPreference = 'SilentlyContinue' #Progress bar makes things way slower
 
 # Ensure that virtualization is enbaled in BIOS.
 Write-Output 'Verifying that virtualization is enabled in BIOS...'
-if ((Get-CimInstance Win32_ComputerSystem).HypervisorPresent -eq $false) {
+if ((Get-CimInstance Win32_ComputerSystem).VirtualizationFirmwareEnabled -eq $false) {
 	Write-Output 'ERROR: Please Enable Virtualization capabilities in your BIOS settings...'
+	exit
+}
+
+# Ensure that virtualization is enbaled in Windows 10.
+Write-Output 'Verifying that virtualization is enabled in Windows 10...'
+if ((Get-CimInstance Win32_ComputerSystem).HypervisorPresent  -eq $false) {
+	Write-Output 'ERROR: Please Enable Hyper-V in your Control Panel->Programs and Features->Turn Windows features on or off'
 	exit
 }
 
