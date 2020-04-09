3f9130e025f015ad02572224cc10f7b2e98a755c
Craig Wilkinson <c.a.wilkinson1@googlemail.com>
2020-04-09 08:42:26 +0100

    Moved module to subfolder
    Manually merged in latest changes, then moved the module to its own folder.  Long term, might be useful to write a script that automatically parses the current script and creates the module from there

---
+++ Folding In Sandbox/Module/FAHSandbox.psm1
@@ -0,0 +1,16 @@
+    $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+    $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+    foreach($import in @($Public + $Private))
+    {
+        Try
+        {
+            . $import.fullname
+        }
+        Catch
+        {
+            Write-Error -Message "Failed to import function $($import.fullname): $_"
+        }
+    }
+
+Export-ModuleMember -Function $Public.Basename
