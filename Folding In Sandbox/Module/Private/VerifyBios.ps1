75f25729f1dec3c47ca31a4f7738730084a508f9
Brandon Smith <Brandon.Smith@microsoft.com>
2020-04-10 11:48:32 -0700

    \#21 Added virtualization check from @cgoeppinger to the ps module.

--- Folding In Sandbox/Module/Private/VerifyBios.ps1
+++ Folding In Sandbox/Module/Private/VerifyBios.ps1
@@ -12,11 +12,18 @@ function VerifyBios
     [cmdletbinding()]
     [OutputType([bool])]
     param ()
-    Write-Verbose 'Verifying that virtualization is enabled in BIOS...';
-    if ((Get-CimInstance Win32_ComputerSystem).HypervisorPresent -eq $false) 
-    {
-        Write-Verbose 'ERROR: Please Enable Virtualization capabilities in your BIOS settings...'
-        return $false;
+    # Ensure that virtualization is enbaled in BIOS.
+    Write-Output 'Verifying that virtualization is enabled in BIOS...'
+    if ((Get-CimInstance Win32_ComputerSystem).VirtualizationFirmwareEnabled -eq $false) {
+        Write-Output 'ERROR: Please Enable Virtualization capabilities in your BIOS settings...'
+        return $false
+    }
+
+    # Ensure that virtualization is enbaled in Windows 10.
+    Write-Output 'Verifying that virtualization is enabled in Windows 10...'
+    if ((Get-CimInstance Win32_ComputerSystem).HypervisorPresent -eq $false) {
+        Write-Output 'ERROR: Please Enable Hyper-V in your Control Panel->Programs and Features->Turn Windows features on or off'
+        return $false
     }
     
     return $true;
